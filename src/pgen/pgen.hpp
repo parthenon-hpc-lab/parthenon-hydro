9fb9a166f750cf09fd545731ebd91c1f9d919e5f
wendelnc <wendelnc@msu.edu>
2025-06-25 10:54:56 -0400

    Add more pgen files

--- src/pgen/pgen.hpp
+++ src/pgen/pgen.hpp
@@ -36,4 +36,48 @@ using namespace parthenon::driver::prelude;
 void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
 } // namespace sod
 
+namespace riemann_2d {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
+} // namespace riemann_2d
+
+namespace brio_wu {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
+} // namespace brio_wu
+
+
+namespace orszag_tang_2d {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
+} // namespace orszag_tang_2d
+
+namespace orszag_tang_3d {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
+} // namespace orszag_tang_3d
+
+namespace turbulence {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(Mesh *pm, parthenon::ParameterInput *pin, MeshData<Real> *md);
+void ProblemInitPackageData(ParameterInput *pin, parthenon::StateDescriptor *pkg);
+void Driving(MeshData<Real> *md, const parthenon::SimTime &tm, const Real dt);
+void SetPhases(MeshBlock *pmb, ParameterInput *pin);
+void UserWorkBeforeOutput(MeshBlock *pmb, ParameterInput *pin,
+                          const parthenon::SimTime &tm);
+void Cleanup();
+} // namespace turbulence
+
+namespace field_loop {
+using namespace parthenon::driver::prelude;
+
+void ProblemGenerator(MeshBlock *pmb, parthenon::ParameterInput *pin);
+void ProblemInitPackageData(ParameterInput *pin, parthenon::StateDescriptor *pkg);
+} // namespace field_loop
+
 #endif // PGEN_PGEN_HPP_
